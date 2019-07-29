c32d7681716f04eb79be6997d114c3a563e7b73a
Rian Dutra <eu@riandutra.com>
2019-07-29 11:59:43 -0300

    Updates

---
+++ precache-manifest.0d3f591def2642e2a8066dcd6abee217.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1515789f73391441ef1c5341908d6d7f",
+    "url": "/har_styleguide/index.html"
+  },
+  {
+    "revision": "d5f1bb59fe7998aada42",
+    "url": "/har_styleguide/static/css/main.60bad551.chunk.css"
+  },
+  {
+    "revision": "ab3416e8106ed66c7ba4",
+    "url": "/har_styleguide/static/js/2.a5aa02e1.chunk.js"
+  },
+  {
+    "revision": "d5f1bb59fe7998aada42",
+    "url": "/har_styleguide/static/js/main.1759b01f.chunk.js"
+  },
+  {
+    "revision": "c60c352c573f7865d4e8",
+    "url": "/har_styleguide/static/js/runtime~main.507d803f.js"
+  },
+  {
+    "revision": "61f9092f7c7638b72e583d729738769a",
+    "url": "/har_styleguide/static/media/_placeholder.61f9092f.svg"
+  },
+  {
+    "revision": "ec254255c9a1eb65b45277346cc4345d",
+    "url": "/har_styleguide/static/media/icon-github.ec254255.svg"
+  },
+  {
+    "revision": "7c8b0da3165fa7237bfebb16e68b8364",
+    "url": "/har_styleguide/static/media/icon-menu-close.7c8b0da3.svg"
+  },
+  {
+    "revision": "d486093378cf48d3e780556477097b73",
+    "url": "/har_styleguide/static/media/icon-menu.d4860933.svg"
+  },
+  {
+    "revision": "66f489207f5a5df4258b94dc5913f7cd",
+    "url": "/har_styleguide/static/media/icon-search.66f48920.svg"
+  },
+  {
+    "revision": "1296296e18c064d1880657991dd99d72",
+    "url": "/har_styleguide/static/media/icon-special-option-arrow-light.1296296e.svg"
+  },
+  {
+    "revision": "0f8d55a882ffde7fbcd8515c8cf21ac5",
+    "url": "/har_styleguide/static/media/logo-har-default.0f8d55a8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7aeedbca8cd2681436ca37829b0cfd47.js"
+  "/har_styleguide/precache-manifest.0d3f591def2642e2a8066dcd6abee217.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/har_styleguide/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
