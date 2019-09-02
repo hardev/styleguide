167d064b28c8a43c7b85382bdfafa799bfde6dd1
Shahid <shahid4u@gmail.com>
2019-09-02 11:26:04 +0500

    added popover with footer

--- src/routes/Components/Popovers.js
+++ src/routes/Components/Popovers.js
@@ -11,7 +11,7 @@ export default class Popovers extends Component {
     <div class="row">
       	<div class="col col-12 pr-sm-5 pl-sm-5">
 		    <div class="position-wrapper">
-		      	<button class="filterpill toggle-popover" data-popover="SelectPrice" data-placement="bottom" data-target="SelectPrice" title="" aria-describedby="SelectPrice">Price</button>
+		      	<button class="filterpill toggle-popover" data-popover="SelectPrice" data-placement="bottom" data-target="SelectPrice" title="" aria-describedby="SelectPrice">Open Popover</button>
 	 	      	<div id="SelectPrice" class="popover popover--standard popover--standard_large" role="tooltip" style="display:none;">
 		      		<h3 class="popover-header text-uppercase text-center">Price</h3>
 		      		<div class="popover-body">
@@ -29,6 +29,31 @@ export default class Popovers extends Component {
 
     </div>`
     ,
+    `
+    <div class="row">
+      	<div class="col col-12 pr-sm-5 pl-sm-5">
+		    <div class="position-wrapper">
+		      	<button class="filterpill toggle-popover" data-popover="Filters" data-placement="bottom" data-target="Filters" title="" aria-describedby="SelectPrice">Open Popover</button>
+	 	      	<div id="Filters" class="popover popover--standard popover--standard_large" role="tooltip" style="display:none;">
+		      		<h3 class="popover-header text-uppercase text-center">Price</h3>
+		      		<div class="popover-body">
+				        Auto Expand and Auto Scroll
+			        </div>
+			        <div class="popover-footer text-right">
+		        		<button role="button" class="btn btn--shapeless btn--medium" aria-label="Search">Reset</button>
+						<button role="button" class="btn btn--primary btn--medium">View 2,030 homes</button>
+			        </div>
+			    </div>
+			</div>
+		    <div class="clearfix"></div>
+		    <div class="component_content__demo__description">Popover Normal Large (with footer)</div>
+        </div>
+
+        <div class="col col-sm-6 col-12 pr-sm-5 pl-sm-5">
+
+        </div>
+    </div>
+	`,
     `<div class="row">
       	<div class="col col-4 pr-sm-5 pl-sm-5">
 		    <div class="position-wrapper">
@@ -72,7 +97,21 @@ export default class Popovers extends Component {
 	        Auto Expand and Auto Scroll
         </div>
     </div>
-</div>`,
+</div>`,`
+<div class="position-wrapper">
+  	<button class="filterpill toggle-popover" data-popover="Filters" data-placement="bottom" data-target="Filters" title="" aria-describedby="SelectPrice">Open Popover with Footer</button>
+      	<div id="Filters" class="popover popover--standard popover--standard_large" role="tooltip" style="display:none;">
+  		<h3 class="popover-header text-uppercase text-center">Price</h3>
+  		<div class="popover-body">
+	        Auto Expand and Auto Scroll
+        </div>
+        <div class="popover-footer text-right">
+    		<button role="button" class="btn btn--shapeless btn--medium" aria-label="Search">Reset</button>
+			<button role="button" class="btn btn--primary btn--medium">View 2,030 homes</button>
+        </div>
+    </div>
+</div>
+`,
 `<!-- Popover Normal Large -->
 <div class="position-wrapper">
   	<button class="custom-select custom-select_large toggle-popover" data-popover="Bedrooms" data-target="Bedrooms" data-placement="bottom" title="" aria-describedby="SelectPrice">Bedrooms</button>
@@ -104,7 +143,8 @@ export default class Popovers extends Component {
         activeChild="popovers">
 
         <TemplateComponentBlock blockTitle="Popover standard" elements={this.elements[0]} codeSnippets={this.codeSnippets[0]} />
-        <TemplateComponentBlock blockTitle="Popover shapeless" elements={this.elements[1]} codeSnippets={this.codeSnippets[1]} />
+        <TemplateComponentBlock blockTitle="Popover standard with Footer" elements={this.elements[1]} codeSnippets={this.codeSnippets[1]} />
+        <TemplateComponentBlock blockTitle="Popover shapeless" elements={this.elements[2]} codeSnippets={this.codeSnippets[2]} />
 
       </TemplateDefault>
     )
